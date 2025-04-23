abf1f003416f1a25c1b605d93cc333ef2a10c9f2
Payrt <payrtovv@gmail.com>
2025-04-22 21:01:50 -0500

    4.0

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -5,7 +5,7 @@ public class Pokemon {
     private String habilidad;
     private String estado;
 
-
+    //Creamos un constructor predefinido
     public Pokemon() {
         this.nombre = "Pikachu";
         this.tipo = "Electrico";
@@ -13,6 +13,7 @@ public class Pokemon {
         this.habilidad = "Impactrueno";
         this.estado = "Activo";
     }
+    //Creamos un constructor por defecto
 
     public Pokemon(String nombre, String tipo, int nivelPoder, String habilidad, String estado) {
         this.nombre = nombre;
@@ -22,6 +23,7 @@ public class Pokemon {
         this.estado = estado;
     }
 
+    //Creamos Getters y setters
     public String getNombre() {
         return nombre;
     }
@@ -50,6 +52,7 @@ public class Pokemon {
         this.nivelPoder = nivelPoder;
     }
 
+    //Creamos el toString
     public String toString() {
         return "Pokemon" + "\n" +
                 "Nombre= " + nombre + '\n' +
